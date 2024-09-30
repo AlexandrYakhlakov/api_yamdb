32a6a4249d6410e65984cf51fb282c86c4e59fda
testqasrt <ayakhlakov@srt-llc.ru>
2024-10-01 02:16:40 +0300

    расширение модели User

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from collections import namedtuple
 
-# Create your models here.
+Role = namedtuple('Role', ('role_value', 'widget_value'))
+admin = Role('admin', 'Администратор')
+moderator = Role('moderator', 'Модератор')
+auth_user = Role('user', 'Пользователь')
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        (*admin,),
+        (*moderator,),
+        (*auth_user,)
+    )
+    bio = models.CharField(
+        blank=True,
+        max_length=500,
+        verbose_name='Биография'
+    )
+    role = models.CharField(
+        choices=ROLE_CHOICES,
+        default=auth_user.role_value,
+        max_length=50,
+        verbose_name='Роль'
+    )
+
+
