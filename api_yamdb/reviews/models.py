d91f6fcf953eb19fe651be166263b17ee0b168b0
testqasrt <ayakhlakov@srt-llc.ru>
2024-10-01 04:09:38 +0300

    написание апи для методов users, фикс модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -14,17 +14,26 @@ class User(AbstractUser):
         (*moderator,),
         (*auth_user,)
     )
+    role = models.CharField(
+        choices=ROLE_CHOICES,
+        default=auth_user.role_value,
+        max_length=50,
+        verbose_name='Роль',
+        blank=True
+    )
+
     bio = models.CharField(
         blank=True,
         max_length=500,
         verbose_name='Биография'
     )
-    role = models.CharField(
-        choices=ROLE_CHOICES,
-        default=auth_user.role_value,
-        max_length=50,
-        verbose_name='Роль'
-    )
+
+    # confirmation_code = models.
+
+    @property
+    def is_admin(self):
+        return self.role == admin.role_value
+
 
     class Meta:
         ordering = ('-id',)
