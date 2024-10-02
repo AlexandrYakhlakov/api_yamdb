6d52b98f661d35dbf31b756800eb53dd3eed524c
Artyom Gadalin <gadalin.artyom@gmail.com>
2024-10-02 23:18:04 +0300

    Add models Review & Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from collections import namedtuple
 
@@ -52,3 +53,48 @@ class User(AbstractUser):
 #         confirmation_code = '1235'
 #         instance.confirmation_code = confirmation_code
 #         instance.save()
+
+class Title(models.Model):
+    pass
+
+class Review(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    score = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        verbose_name='Рейтинг произведения'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews'
+    )
+    title = models.OneToOneField(
+        Title, on_delete=models.CASCADE, related_name='reviews'
+    )
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments'
+    )
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
