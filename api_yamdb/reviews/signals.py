f7067725f0664d64f874f1b7fdb2a051df8bf66c
testqasrt <ayakhlakov@srt-llc.ru>
2024-10-04 22:39:56 +0300

    Фикс

--- api_yamdb/reviews/signals.py
+++ api_yamdb/reviews/signals.py
@@ -1,9 +1,10 @@
+import uuid
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import User
 
-import uuid
 
 @receiver(post_save, sender=User)
 def post_save(instance, created, **kwargs):
