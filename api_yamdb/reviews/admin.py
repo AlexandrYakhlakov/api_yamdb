a7f47ae10d95fef7c0d860ca9595bf2d3ce3a620
testqasrt <ayakhlakov@srt-llc.ru>
2024-10-04 22:41:37 +0300

    Фикс

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import User
+
+from reviews.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    # list_display = ('__all__',)
     ...
