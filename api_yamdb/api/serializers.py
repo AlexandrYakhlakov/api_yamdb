6b7a2bcb6803de985984103d10313a7e9e22e8ed
testqasrt <ayakhlakov@srt-llc.ru>
2024-10-11 16:44:28 +0300

    фикс

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -118,7 +118,7 @@ class TitleSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
         )
-        read_only_fields = ('__all__',)
+        read_only_fields = fields
 
 
 class TitleWriteSerializer(serializers.ModelSerializer):
